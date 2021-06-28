7fbaa8b19adf231b3c4323ae729d11e38e238031
DalekCraft2 <ericdalekcraft@gmail.com>
2021-06-28 00:37:00 -0400

    Cleaned a bit and got the Javadoc plugin to work

--- src/main/java/me/eccentric_nz/tardisschematicviewer/JsonObject.java
+++ src/main/java/me/eccentric_nz/tardisschematicviewer/JsonObject.java
@@ -400,7 +400,7 @@ public class JsonObject {
 
     /**
      * Produce a string in double quotes with backslash sequences in all the
-     * right places. A backslash will be inserted within </, producing <\/,
+     * right places. A backslash will be inserted within {@code </}, producing {@code <\/},
      * allowing JSON text to be delivered in HTML. In JSON text, a string cannot
      * contain a control character or an unescaped quote or backslash.
      *
